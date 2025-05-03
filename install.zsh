70f685e128a715d8296574d952df8fedde443bf0
Declan Zevan <declan.zevan@gmail.com>
2025-05-03 15:12:20 -0400

    Added more dependency checks

--- install.zsh
+++ install.zsh
@@ -25,10 +25,17 @@ BOLD_RESET="\033[0m\033[1m"
 printf "\nWelcome to ${BOLD_MAGENTA}D3CL4NZ's dotfile installation script${RESET}!\n"
 printf "[${BOLD_YELLOW}!${RESET}] ${BOLD_CYAN}This script is meant to be executed on ArchLinux.${RESET}\n\n"
 
+# Check if git is installed
 printf "[${BOLD_YELLOW}•${RESET}] Checking for git..."
 command -v git > /dev/null 2>&1 || { printf >&2 "\r[${BOLD_RED}✘${RESET}] ${BOLD_MAGENTA}git${RESET} ${RED}is not installed.${RESET}\n"; exit 1; }
 printf "\r[${BOLD_GREEN}✔${RESET}] Checking for git...\n"
 
+# Check if alacritty is installed
+printf "[${BOLD_YELLOW}•${RESET}] Checking for alacritty..."
+command -v alacritty > /dev/null 2>&1 || { printf >&2 "\r[${BOLD_RED}✘${RESET}] ${BOLD_MAGENTA}alacritty${RESET} ${RED}is not installed.${RESET}\n"; exit 1; }
+printf "\r[${BOLD_GREEN}✔${RESET}] Checking for alacritty...\n"
+
+# Check if oh-my-zsh is installed
 printf "[${BOLD_YELLOW}•${RESET}] Checking for oh-my-zsh..."
 if [ -d "$HOME/.oh-my-zsh" ]; then
     printf "\r[${BOLD_GREEN}✔${RESET}] Checking for oh-my-zsh...\n"
@@ -37,6 +44,7 @@ else
     exit 1
 fi
 
+# Check if powerlevel10k is installed
 printf "[${BOLD_YELLOW}•${RESET}] Checking for powerlevel10k..."
 if [ -d "$HOME/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
     printf "\r[${BOLD_GREEN}✔${RESET}] Checking for powerlevel10k...\n"
@@ -45,19 +53,54 @@ else
     exit 1
 fi
 
+# Check if i3 is installed
 printf "[${BOLD_YELLOW}•${RESET}] Checking for i3..."
 command -v i3 > /dev/null 2>&1 || { printf >&2 "\r[${BOLD_RED}✘${RESET}] ${BOLD_MAGENTA}i3${RESET} ${RED}is not installed.${RESET}\n"; exit 1; }
 printf "\r[${BOLD_GREEN}✔${RESET}] Checking for i3...\n"
 
+# Check if fontconfig is installed
 printf "[${BOLD_YELLOW}•${RESET}] Checking for fontconfig..."
 command -v git > /dev/null 2>&1 || { printf >&2 "\r[${BOLD_RED}✘${RESET}] ${BOLD_MAGENTA}fontconfig${RESET} ${RED}is not installed.${RESET}\n"; exit 1; }
 printf "\r[${BOLD_GREEN}✔${RESET}] Checking for fontconfig...\n"
 
+# Check if Meslo Nerd Fonts are installed
 printf "[${BOLD_YELLOW}•${RESET}] Checking for ttf-meslo-nerd..."
 if fc-list | grep -i "MesloLGM Nerd Font" >/dev/null 2>&1; then
   printf "\r[${BOLD_GREEN}✔${RESET}] Checking for ttf-meslo-nerd...\n"
 else
   printf "\r[${BOLD_RED}✘${RESET}] ${BOLD_MAGENTA}ttf-meslo-nerd${RESET} ${RED}is not installed.${RESET}\n"
+  exit 1
+fi
+
+# Check if zsh-syntax-highlighting is installed
+printf "[${BOLD_YELLOW}•${RESET}] Checking for zsh-syntax-highlighting..."
+if [ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+    printf "\r[${BOLD_GREEN}✔${RESET}] Checking for zsh-syntax-highlighting...\n"
+else
+    printf "\r[${BOLD_RED}✘${RESET}] ${BOLD_MAGENTA}zsh-syntax-highlighting${RESET} ${RED}is not installed.${RESET}\n"
+    exit 1
+fi
+
+# Check if zsh-autosuggestions is installed
+printf "[${BOLD_YELLOW}•${RESET}] Checking for zsh-autosuggestions..."
+if [ -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+    printf "\r[${BOLD_GREEN}✔${RESET}] Checking for zsh-autosuggestions...\n"
+else
+    printf "\r[${BOLD_RED}✘${RESET}] ${BOLD_MAGENTA}zsh-autosuggestions${RESET} ${RED}is not installed.${RESET}\n"
+    exit 1
+fi
+
+# Check if rubygems is installed
+printf "[${BOLD_YELLOW}•${RESET}] Checking for gem..."
+command -v gem > /dev/null 2>&1 || { printf >&2 "\r[${BOLD_RED}✘${RESET}] ${BOLD_MAGENTA}gem${RESET} ${RED}is not installed.${RESET}\n"; exit 1; }
+printf "\r[${BOLD_GREEN}✔${RESET}] Checking for gem...\n"
+
+# Check if colorls is installed
+printf "[${BOLD_YELLOW}•${RESET}] Checking for colorls..."
+if gem list --local | grep -i "colorls" >/dev/null 2>&1; then
+  printf "\r[${BOLD_GREEN}✔${RESET}] Checking for colorls...\n"
+else
+  printf "\r[${BOLD_RED}✘${RESET}] ${BOLD_MAGENTA}colorls${RESET} ${RED}is not installed.${RESET}\n"
   exit 1
 fi
 
@@ -101,5 +144,5 @@ else
 fi
 printf "\r[${BOLD_GREEN}✔${RESET}] Updating zshrc...\n"
 
-printf "\n[${BOLD_GREEN}✔${RESET}] ${BOLD_GREEN}Installation completed without errors.${RESET}\n\n"
+printf "\n[${BOLD_GREEN}了${RESET}] ${BOLD_GREEN}Installation completed without errors.${RESET}\n\n"
 exit 0
